d50315a9dbc83331adc260802b7f689bc879f25c
Chris <github.account@chrigel.net>
2021-11-05 21:55:06 +0100

    Print the changelog config

--- .github/changelog.sh
+++ .github/changelog.sh
@@ -9,8 +9,7 @@ chart=$(echo ${tag} | grep -P -o '^([a-zA-Z0-9-]+)(?![0-9.]+)')
 tagPattern="${chart}-(.+)"
 labelPattern='^(?!\\['${chart}'\\])'
 
-echo "Tag search pattern: $tagPattern"
-echo "Label search pattern: $labelPattern"
-
+echo ::group::Configuring changelog generator
 cat .github/changelog-configuration.json | jq '.tag_resolver.filter.pattern="'$tagPattern'" | .label_extractor[0].pattern="'$labelPattern'"' > .github/configuration.json
-# cat .github/configuration.json
+cat .github/configuration.json
+echo ::endgroup::
