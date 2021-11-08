a1cabe95e53232e7486edb14db8bc906828c4e0d
Chris <github.account@chrigel.net>
2021-11-08 10:42:03 +0100

    Test the fix of changelog generator
    Signed-off-by: Chris <github.account@chrigel.net>

--- .github/changelog.sh
+++ .github/changelog.sh
@@ -10,6 +10,6 @@ tagPattern="${chart}-(.+)"
 labelPattern='^(?!\\['${chart}'\\])'
 
 echo ::group::Configuring changelog generator
-cat .github/changelog-configuration.json | jq '.tag_resolver.filter.pattern="'$tagPattern'" | .label_extractor[0].pattern="'$labelPattern'"' > .github/configuration.json
+cat .github/changelog-configuration.json | jq '.tag_resolver.filter.pattern="'$tagPattern'" | .tag_resolver.transformer.pattern="'$tagPattern'" | .label_extractor[0].pattern="'$labelPattern'"' > .github/configuration.json
 cat .github/configuration.json
 echo ::endgroup::
